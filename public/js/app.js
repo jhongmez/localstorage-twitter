78a424bf6fac6d7dc833ee636156da3af1b922ac
Jhon Gomez Marín <gmezjhon@gmail.com>
2020-05-04 21:16:09 -0500

    Agregamos el botón borrar

--- public/js/app.js
+++ public/js/app.js
@@ -20,15 +20,25 @@ function agregarTweet(e) {
     // Con el prevent default hacemos que la pagina no se nos recargue
     e.preventDefault();
 
+
     // creamos una varibale y en ella obtenemos el ID del textarea con el value
     const tweet = document.getElementById("tweet").value;
 
+
+    // creamos el boton de eliminar
+    const botonBorrar = document.createElement('a');
+    botonBorrar.classList = 'borrar-tweet';  
+    botonBorrar.textContent = 'X';
+
+
     // creamos una varible la cual nos va a agregar un elemento <li></li>
     const lista = document.createElement('li');
-
-    // A esta variable le agregamos la propiedad innerText la cual nos coloca el texto
-    lista.innerText = tweet;
-
+    lista.textContent = tweet;
     // A esta variable le agregamos la propiedad appendChild la cual hace que su hijo sea lo que contiene lista
     listaTweets.appendChild(lista);
+    // agregamos en boton borrar con appendChild
+    lista.appendChild(botonBorrar);
+
+
+    console.log(listaTweets);
 }
